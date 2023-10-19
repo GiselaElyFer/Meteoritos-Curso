9937c9c87e22af40b3bba5838638ce8b695539c5
Gisela <fernandezelisabet86@gmail.com>
2023-10-19 19:32:40 -0300

    Nave-disparando
    Se agrego:
    - caniones
    - proyectiles
    -autoloads
    -y actualizo player.
    la nave se mueve, gira y dispara

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,4 +1,5 @@
 class_name Player
+
 extends RigidBody2D
 
 export var potencia_motor: int = 20
@@ -7,6 +8,9 @@ export var potencia_rotacion: int = 280
 
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion : int = 0
+
+
+onready var canion:Canion = $Canion
 
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
@@ -18,6 +22,7 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 
 func _process(delta: float) -> void:
 	player_input()
+	
 
 
 func player_input() -> void:
@@ -33,3 +38,9 @@ func player_input() -> void:
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
+	
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
