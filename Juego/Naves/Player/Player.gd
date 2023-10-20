24d5e233a217e8f0d5d5ecbe6add9e9a54d769cc
Gisela <fernandezelisabet86@gmail.com>
2023-10-20 11:47:51 -0300

    player y enemigos
    se creo el enemigo dummy
    -aparecieron algunos errores en la nave a solucionar pero no afecta su ejecucion, mas bien es de diseño

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -2,16 +2,27 @@ class_name Player
 
 extends RigidBody2D
 
+
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
+
 export var potencia_motor: int = 20
 export var potencia_rotacion: int = 280
-
+export var estela_maxima: int = 150
 
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion : int = 0
+var estado_actual:int = ESTADO.SPAWN
 
 
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
+
+
+#func _ready() -> void:
+	#controlador_estados(estado_actual)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -21,18 +32,52 @@ func _unhandled_input(event: InputEvent) -> void:
 	
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
+	
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(0)
+		motor_sfx.sonido_on()
+	if(event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
+		motor_sfx.sonido_off()
+
+
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			canion.set.puede_disparar(true)
+			colisionador.set_deferred("disabled", false)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set.puede_disparar(false)
+			Eventos.emit_signal("nave_destruida", global_position)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	
+	estado_actual = nuevo_estado
+
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	
+	return true
 
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-	rad2deg(rotation)
-	empuje.rotated(rotation)
-	apply_central_impulse(empuje)
+	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
 
 
 func _process(delta: float) -> void:
 	player_input()
-	
 
 
 func player_input() -> void:
@@ -54,3 +99,10 @@ func player_input() -> void:
 	
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
+
+
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	if anim_name == "spawn":
+		controlador_estados(ESTADO.VIVO)
--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -3,7 +3,11 @@ class_name Nivel
 extends Node2D
 
 
+export var explosion:PackedScene = null
+
+
 onready var contenedor_proyectiles:Node
+
 
 func _ready() -> void:
 	conectar_seniales()
@@ -12,6 +16,13 @@ func _ready() -> void:
 
 func conectar_seniales() -> void:
 	Eventos.connect("disparo", self, "_on_disparo")
+	Eventos.connect("nave_destruida", self, "_on_nave_destruida")
+
+
+func _on_nave_destruida(posicion: Vector2) -> void:
+	var new_explosion:Node2D = explosion.instance()
+	new_explosion.global_position = posicion
+	add_child(new_explosion)
 
 
 func crear_contenedores() -> void:
@@ -22,3 +33,5 @@ func crear_contenedores() -> void:
 
 func _on_disparo(proyectil:Proyectil) -> void:
 	contenedor_proyectiles.add_child(proyectil)
+
+
